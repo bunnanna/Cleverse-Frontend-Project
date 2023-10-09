a102ac17f40ad8569036891dbb204784314218d1
bunnana <na.nuttanont@gmail.com>
2023-10-09 11:03:11 +0700

    add and config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import FullPages from './template/FullPages'
+import MainPage from './pages/MainPage'
+
 function App() {
-  return <></>
+  return (
+    <Routes>
+      <Route path="/" element={<FullPages />}>
+        <Route index element={<MainPage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
