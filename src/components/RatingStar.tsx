aee92985a42d841c62fd60db120b0c9289f7459e
bunnana <na.nuttanont@gmail.com>
2023-10-10 11:32:20 +0700

    change RatingStar state

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -1,19 +1,27 @@
+import { useState } from 'react'
+
 interface IRatingStarProps {
   rating: number
-  changeable: boolean
+  setRating?: (ratingNum: number) => void | null
+  size?: 'rating-xs' | 'rating-sm' | 'rating-md' | 'rating-xl'
 }
-const RatingStar = ({ rating, changeable }: IRatingStarProps) => {
+const RatingStar = ({ rating, setRating, size = 'rating-md' }: IRatingStarProps) => {
+  const [currentRating, setCurrentRating] = useState(rating + 1)
+  const changeable = !!setRating
+
   return (
-    <div className="rating rating-sm">
+    <div className={`rating ${size}`}>
       {[...new Array(5)].map((_, idx) => {
         return (
           <input
             key={idx}
             type="radio"
             name="rating-2"
-            className={`mask mask-star-2 ${rating > idx + 1 ? 'bg-orange-400' : 'bg-gray-400'} ${
+            className={`mask mask-star-2 ${currentRating >= idx + 1 ? 'bg-orange-400' : 'bg-gray-400'} ${
               changeable ? '' : 'cursor-default'
             } `}
+            onMouseEnter={() => changeable && setCurrentRating(idx + 1)}
+            onClick={() => changeable && setRating(currentRating)}
           />
         )
       })}
