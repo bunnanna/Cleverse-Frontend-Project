616c5962ad5ddee66e58d0243bcb1044d40e0164
bunnana <na.nuttanont@gmail.com>
2023-10-10 11:35:17 +0700

    add toast to full page template

--- src/template/FullPages.tsx
+++ src/template/FullPages.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Toast from '../components/Toast'
+import { useUser } from '../provider/AuthProvider'
 
 const FullPages = () => {
+  const { isLoading, isError } = useUser()
   return (
     <div className="flex flex-col justify-center items-center flex-1">
-      <Toast />
+      <Toast isLoading={isLoading} isError={isError} />
       <Navbar />
       <div className="bg-white w-full flex flex-col justify-center items-center">
         <Outlet />
